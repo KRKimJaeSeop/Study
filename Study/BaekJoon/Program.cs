cc09ff0496321ed5ea84dddfcf15b4649151de26
KimJaeSeop <greenseop@gmail.com>
2023-12-21 18:29:42 +0900

    3. 1001

--- Study/BaekJoon/Program.cs
+++ Study/BaekJoon/Program.cs
@@ -3,20 +3,27 @@
     private static void Main(string[] args)
     {
 
-        Q1000();
+        Q1001();
 
         void Q2557()
         {
             Console.WriteLine("Hello World!");
 
         }
-
         void Q1000()
         {
             var input = Console.ReadLine() as string;
             var A = int.Parse(input.Split(" ")[0]);
             var B = int.Parse(input.Split(" ")[1]);
-            Console.WriteLine(A+B);
+            Console.WriteLine(A + B);
+        }
+
+        void Q1001()
+        {
+            var input = Console.ReadLine() as string;
+            var A = int.Parse(input.Split(" ")[0]);
+            var B = int.Parse(input.Split(" ")[1]);
+            Console.WriteLine(A - B);
         }
 
     }
