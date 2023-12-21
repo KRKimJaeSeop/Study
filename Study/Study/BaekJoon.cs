561d2a8dc8e0c4c042780371f48f8365f823d13b
KimJaeSeop <greenseop@gmail.com>
2023-12-21 18:22:53 +0900

    2. 1000

---
+++ Study/Study/BaekJoon.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Q1000();
+
+        void Q2557()
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+
+        void Q1000()
+        {
+            var input = Console.ReadLine() as string;
+            var A = int.Parse(input.Split(" ")[0]);
+            var B = int.Parse(input.Split(" ")[1]);
+            Console.WriteLine(A+B);
+        }
+
+    }
+}
