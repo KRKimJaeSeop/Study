646367f6697c2ef0b28087c2f377028dc3197ea0
KimJaeSeop <greenseop@gmail.com>
2023-12-21 18:06:28 +0900

    upload file

---
+++ Study/Study/1000.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+public class Class1
+{
+	public Class1()
+	{
+        var input = Console.ReadLine() as string;
+        var A = int.Parse(input.Split(" ")[0]);
+        var B = int.Parse(input.Split(" ")[1]);
+        Console.WriteLine(A + B);
+
+    }
+}
