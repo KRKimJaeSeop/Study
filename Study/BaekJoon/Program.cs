6bef68b8044e1cb186ee7666e778ca45405528bf
KimJaeSeop <greenseop@gmail.com>
2023-12-26 19:28:33 +0900

    11.과일장수

--- Study/BaekJoon/Program.cs
+++ Study/BaekJoon/Program.cs
@@ -3,7 +3,7 @@
     private static void Main(string[] args)
     {
 
-        Q1330();
+        FruitDealer();
 
         //1
         void Q2557()
@@ -113,6 +113,64 @@
                 Console.WriteLine("==");
 
             }
+        }
+
+        void FruitDealer()
+        {
+            while (true)
+            {
+
+                //입력은 1 2 3,4,5,6 이런식으로.
+                string input = Console.ReadLine();
+                //제출할 답
+                int answer = 0;
+
+                // k는 사과의 최고등급 점수다.
+                int k = int.Parse(input.Split(" ")[0]);
+                // m은 한상자에 들어가는 사과 갯수다.
+                int m = int.Parse(input.Split(" ")[1]);
+                // score는 사과의 점수 리스트다.
+                int[] score = input.Split(" ")[2].Split(',').Select(n => Convert.ToInt32(n)).ToArray();
+                // score 정렬
+                Array.Sort(score);
+                Array.Reverse(score);
+
+                // 박스의 갯수를 구한다.
+                var boxAmount = score.Length / m;
+                // 수가 딱떨어지지 않으면 박스를 하나 더한다.
+                if (score.Length % m > 0)
+                    boxAmount++;
+
+                // 큰 배열이 박스들, 작은배열은 박스안의 사과들
+                List<List<int>> boxes = new List<List<int>>();
+
+                // 박스배열 세팅.
+                for (int i = 0; i < boxAmount; i++)
+                {
+                    boxes.Add(new List<int>());
+                }
+
+                // 사과번호 순서대로 박스에 넣는다. 넣을 박스는 번호/m
+                for (int i = 0; i < score.Length; i++)
+                {
+                    var a = i / m;
+                    boxes[a].Add(score[i]);
+                }
+
+
+                foreach (var box in boxes)
+                {
+                    if(box.Count == m)
+                    {
+                        var a = box[box.Count-1] * box.Count;
+                        answer += a;
+                    }
+              
+                }
+
+            }
+
+
         }
     }
 }
